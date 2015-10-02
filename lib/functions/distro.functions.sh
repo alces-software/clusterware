ecd1e669e6cb2a68a08eb5dea1f3cd279e9cabfb
Mark J. Titorenko <mark.titorenko@alces-software.com>
2015-10-02 16:52:06 +0100

    Library functions for distro handling, serf member handling and ruby helpers.

---
+++ lib/functions/distro.functions.sh
@@ -0,0 +1,46 @@
+#==============================================================================
+# Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Clusterware.
+#
+# Alces Clusterware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Clusterware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Clusterware, please visit:
+# https://github.com/alces-software/clusterware
+#==============================================================================
+if [ -z "${cw_DIST}" ]; then
+    source "${cw_ROOT}/etc/distro.vars.sh"
+fi
+
+distro_enable_service() {
+    local service
+    service="$1"
+    # Enable serf service for future boots and start it up
+    if [ "${cw_DIST}" == "el7" ]; then
+        systemctl enable ${service}
+    elif [ "${cw_DIST}" == "el6" ]; then
+        chkconfig ${service} on
+    fi
+}
+
+distro_start_service() {
+    local service
+    service="$1"
+    # Enable serf service for future boots and start it up
+    if [ "${cw_DIST}" == "el7" ]; then
+        systemctl start ${service}
+    elif [ "${cw_DIST}" == "el6" ]; then
+        service ${service} start
+    fi
+}
---
+++ lib/functions/member.functions.sh
@@ -0,0 +1,59 @@
+#==============================================================================
+# Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Clusterware.
+#
+# Alces Clusterware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Clusterware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Clusterware, please visit:
+# https://github.com/alces-software/clusterware
+#==============================================================================
+cw_MEMBER_DIR="${cw_ROOT}"/var/lib/members
+
+member_register() {
+    local member name address role tags
+
+    # parse stdin for new member data
+    member=($(cat))
+    name="${member[0]}"
+    address="${member[1]}"
+    role="${member[2]}"
+    tags="${member[3]}"
+
+    if [ ! -f "${cw_MEMBER_DIR}"/"${name}" ]; then
+        mkdir -p "${cw_MEMBER_DIR}"
+        cat <<EOF > "${cw_MEMBER_DIR}"/"${name}"
+cw_MEMBER_address="${address}"
+cw_MEMBER_role="${role}"
+cw_MEMBER_tags="${tags}"
+EOF
+}
+
+member_unregister() {
+    local member name
+    member=($(cat))
+    name="${member[0]}"
+    if [ -f "${cw_MEMBER_DIR}"/"${name}" ]; then
+        rm -f "${cw_MEMBER_DIR}"/"${name}"
+    fi
+}
+
+member_count() {
+    ls -A "${cw_MEMBER_DIR}" | wc -l
+}
+
+member_quorum() {
+    . "${cw_ROOT}"/etc/config/cluster.vars.sh
+    echo "${cw_CLUSTER_QUORUM}"
+}
