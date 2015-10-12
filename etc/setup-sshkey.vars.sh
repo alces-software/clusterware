ef743d7799e2cab36c167c1d20cb67ea08f9558f
Mark J. Titorenko <mark.titorenko@alces-software.com>
2015-10-12 11:35:16 +0100

    Add ssh keypair generation.  Fixes #3.

---
+++ etc/setup-sshkey.vars.sh
@@ -0,0 +1,11 @@
+################################################################################
+##
+## Alces Clusterware - Shell configuration
+## Copyright (c) 2015 Alces Software Ltd
+##
+################################################################################
+cw_SSH_LOWEST_UID=500
+cw_SSH_SKIP_USERS="root alces"
+cw_SSH_KEYNAME=id_alcescluster
+cw_SSH_DIR="$HOME/.ssh"
+cw_SSH_LOG="$HOME/clusterware-setup-sshkey.log"
