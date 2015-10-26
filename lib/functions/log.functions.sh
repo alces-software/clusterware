9032e2100c7f488d2e1a29fd32b20ad4a56f1c3c
Mark J. Titorenko <mark.titorenko@alces-software.com>
2015-10-26 12:20:21 +0000

    Added some functions to aid with simple logging.

---
+++ lib/functions/log.functions.sh
@@ -0,0 +1,52 @@
+#==============================================================================
+# Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
+#
+# This file/package is part of Alces Clusterware.
+#
+# Alces Clusterware is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Alces Clusterware is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more information on the Alces Clusterware, please visit:
+# https://github.com/alces-software/clusterware
+#==============================================================================
+log() {
+    local message logfile
+    message="$1"
+    logfile="$2"
+    if [ "$logfile" == '-' ]; then
+        echo "$(date +"%b %e %H:%M:%S") ${message}"
+    elif [ "$logfile" ]; then
+        echo "$(date +"%b %e %H:%M:%S") ${message}" >> "$logfile"
+    else
+        echo "$(date +"%b %e %H:%M:%S") ${message}" > /dev/stderr
+    fi
+}
+
+log_blob() {
+    local logfile prefix message date
+    logfile="$1"
+    prefix="$2"
+    date="$(date +"%b %e %H:%M:%S")"
+    if [ "$prefix" ]; then
+        prefix="${date} [${prefix}]"
+    else
+        prefix="${date}"
+    fi
+    if [ "$logfile" == '-' ]; then
+        cat | sed "s/^/${prefix} /g"
+    elif [ "$logfile" ]; then
+        cat | sed "s/^/${prefix} /g" >> "$logfile"
+    else
+        cat | sed "s/^/${prefix} /g" > /dev/stderr
+    fi
+}
