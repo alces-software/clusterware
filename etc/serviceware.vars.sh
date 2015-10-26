345564a5a20f460d45f699a9f857baaf158bd6f3
Mark J. Titorenko <mark.titorenko@alces-software.com>
2015-10-26 12:18:17 +0000

    Provide propagation of serviceware tarballs.

---
+++ etc/serviceware.vars.sh
@@ -0,0 +1,7 @@
+################################################################################
+##
+## Alces Clusterware - Shell configuration
+## Copyright (c) 2015 Alces Software Ltd
+##
+################################################################################
+cw_DIST_URL=https://s3-eu-west-1.amazonaws.com/packages.alces-software.com/clusterware/dist
