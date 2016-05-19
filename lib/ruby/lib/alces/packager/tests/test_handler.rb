d7e18ba70ead0682f27d9e670bb9fc1c4821f621
Bob Whitelock <bob.whitelock@alces-software.com>
2016-05-19 18:01:21 +0100

    Test HandlerProxy calls Handler method

--- lib/ruby/lib/alces/packager/tests/test_handler.rb
+++ lib/ruby/lib/alces/packager/tests/test_handler.rb
@@ -1,10 +1,26 @@
 
 require 'minitest/autorun.rb'
+require 'bourne'
 require 'mocha/mini_test'
 
 require 'alces/packager/cli'
 
 class TestHandlerProxy < MiniTest::Test
+  describe 'method_missing' do
+    def test_calls_handler_method
+      handler_args = [[], Commander::Command::Options.new]
+      spied_handler = Alces::Packager::Handler.new(*handler_args)
+      spied_handler.stubs(:install)
+      Alces::Packager::Handler.stubs(:new).returns(spied_handler)
+
+      handler_proxy = Alces::Packager::HandlerProxy.new
+      handler_proxy.install(*handler_args)
+
+      assert_received(Alces::Packager::Handler, :new)
+      assert_received(spied_handler, :install)
+    end
+  end
+
   describe 'last_update_time' do
     def setup
       stub_last_update_file = StringIO.new(Time.new(2016, 5, 19).to_s)
