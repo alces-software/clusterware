08849318ea52af35497f45df32b65a6e770e587c
Mark Titorenko <mark.titorenko@alces-software.com>
2016-05-24 19:56:24 +0100

    Latest build scripts/dockerfiles.

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+version=$1
 docker login
-docker build --rm=true -t "alces/clusterware-el7:1.4.0" el7-1.4.0
-docker push "alces/clusterware-el7:1.4.0"
-docker build --rm=true -t "alces/clusterware-el6:1.4.0" el6-1.4.0
-docker push "alces/clusterware-el6:1.4.0"
+docker build --rm=true -t "alces/clusterware-el7:${version}" el7-${version}
+docker push "alces/clusterware-el7:${version}"
+docker build --rm=true -t "alces/clusterware-el6:${version}" el6-${version}
+docker push "alces/clusterware-el6:${version}"
