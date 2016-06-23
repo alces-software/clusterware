87712b8e7f061aa5a2582aaa653d8719610f93b5
Mark Titorenko <mark.titorenko@alces-software.com>
2016-06-23 17:08:57 +0100

    Reorder &&s and ||s for great justice.

--- lib/functions/network.functions.sh
+++ lib/functions/network.functions.sh
@@ -145,8 +145,8 @@ network_cidr_to_mask() {
 }
 
 network_is_ec2() {
-    [ "${cw_TEST_ec2}" == "true" ] ||
-        [ -f /sys/hypervisor/uuid ] && [ "$(head -c3 /sys/hypervisor/uuid)" == "ec2" ]
+    [ -f /sys/hypervisor/uuid ] && [ "$(head -c3 /sys/hypervisor/uuid)" == "ec2" ] ||
+        [ "${cw_TEST_ec2}" == "true" ]
 }
 
 network_fetch_ec2_document() {
