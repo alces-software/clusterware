4f19fa3086857b37607cf055d493c7f8ce78a8bc
william <william.mccumstie@outlook.com>
2017-05-09 15:54:27 +0100

    Clocksource changes on master node are replicated accross the cluster

--- lib/functions/clocksource.functions.sh
+++ lib/functions/clocksource.functions.sh
@@ -23,9 +23,11 @@
 require action
 require ui
 require files
+require member
 
 cw_CLOCKSOURCE_current_file=/sys/devices/system/clocksource/clocksource0/current_clocksource
 cw_CLOCKSOURCE_config_file=${cw_ROOT}/etc/cluster-clocksource.rc
+cw_CLOCKSOURCE_sym_link=${cw_ROOT}/var/lib/alces-flight-www/customizer/cluster-clocksource.rc
 
 _clocksource_write_config() {
 	local default="$1" clocksource="$2"
@@ -68,30 +70,70 @@ clocksource_list() {
 	done
 }
 
-clocksource_set_default() {
-	if files_load_config --optional cluster-clocksource; then
-		clocksource_set_source "$cw_CLUSTER_default_clocksource" "true"
-		rm -f "$cw_CLOCKSOURCE_config_file"
+_clocksource_is_master_node() {
+	if [[ -z "$cw_INSTANCE_role" ]]; then
+		files_load_config instance config/cluster
+	fi
+	if [[ "$cw_INSTANCE_role" == "master" ]]; then
+		return 0
+	else
+		return 1
 	fi
 }
 
-clocksource_set_source() {
-	local input=$1 skip_write_config=$2 validated default
+_clocksource_set_default() {
+	if files_load_config --optional cluster-clocksource; then
+		_clocksource_set_source "$cw_CLUSTER_default_clocksource" "true"
+		rm -f $cw_CLOCKSOURCE_config_file $cw_CLOCKSOURCE_sym_link
+	fi
+
+}
+
+_clocksource_set_source() {
+	local input="$1" skip_write_config="$2" validated default
 	shift 2
 
 	validated=$(clocksource_get_available | grep -wo "$input")
 	if [[ -z $validated ]]; then
 		action_die "unrecognised clocksource: $input"
 	fi
+	echo $validated > $cw_CLOCKSOURCE_current_file
 
-	if files_load_config --optional cluster-clocksource; then
-		default="$cw_CLUSTER_default_clocksource"
+	if [[ "$skip_write_config" != "true" ]]; then
+		if files_load_config --optional cluster-clocksource; then
+			default="$cw_CLUSTER_default_clocksource"
+		else
+			default=$(cat $cw_CLOCKSOURCE_current_file)
+		fi
+
+		_clocksource_write_config "$default" "$validated"
+		
+		if _clocksource_is_master_node; then
+			ln -fs $cw_CLOCKSOURCE_config_file $cw_CLOCKSOURCE_sym_link
+		fi
+	fi
+}
+
+_clocksource_ssh_compute_nodes() {
+	local master_node="$1" cmd="$2" node="$4"
+	shift 3
+
+	if [[ "$master_node" != "$node" ]]; then
+		ssh $node alces configure clocksource $cmd
+	fi
+}
+
+clocksource_set() {
+	local cmd="$1"
+	shift
+
+	if [[ "$cmd" == "default" ]]; then
+		_clocksource_set_default
 	else
-		default=$(cat $cw_CLOCKSOURCE_current_file)
+		_clocksource_set_source "$cmd"
 	fi
 
-	echo $validated > $cw_CLOCKSOURCE_current_file
-	if [[ -z "$skip_write_config" ]]; then
-		_clocksource_write_config "$default" "$validated"
+	if _clocksource_is_master_node; then
+		member_each _clocksource_ssh_compute_nodes $(hostname) "$cmd"
 	fi
 }
