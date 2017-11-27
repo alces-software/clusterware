d5e1876d86e92b2b7738a7e6f9ca61701a4549da
Mark J. Titorenko <mark.titorenko@alces-software.com>
2017-11-27 11:33:46 +0000

    Further extraction to serviceware.

--- etc/profile.d/80-sshkey.sh
+++
@@ -1,9 +0,0 @@
-################################################################################
-##
-## Alces Clusterware - Shell configuration
-## Copyright (c) 2015 Alces Software Ltd
-##
-################################################################################
-if [ -x "${cw_ROOT}"/libexec/share/setup-sshkey ]; then
-    "${cw_ROOT}"/libexec/share/setup-sshkey
-fi
