312e4d6317ec2d862370b0ae51183609f3098472
Mark J. Titorenko <mark.titorenko@alces-software.com>
2018-01-19 12:31:39 +0000

    Read file in `/sys` rather than using `dmidecode` (as the latter only works for the root user).

--- lib/functions/network.functions.sh
+++ lib/functions/network.functions.sh
@@ -176,8 +176,8 @@ network_cidr_to_mask() {
 
 network_is_ec2() {
   [ -f /sys/hypervisor/uuid ] && [ "$(head -c3 /sys/hypervisor/uuid)" == "ec2" ] ||
-      [ "$(dmidecode -s baseboard-manufacturer)" == "Amazon Corporate LLC" ] ||
-      [ "$(dmidecode -s baseboard-manufacturer)" == "Amazon EC2" ] ||
+      [ "$(cat /sys/devices/virtual/dmi/id/board_vendor)" == "Amazon Corporate LLC" ] ||
+      [ "$(cat /sys/devices/virtual/dmi/id/board_vendor)" == "Amazon EC2" ] ||
       [ "${cw_TEST_ec2}" == "true" ]
 }
 
